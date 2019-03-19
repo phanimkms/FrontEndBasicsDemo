20741d22b3040e05cb5e8996fec8aadcd1fd2953
phanimkms <phanimkms@gmail.com>
2019-03-20 03:36:47 +0530

    Modal first draft commit

--- stub/js/index.js
+++ stub/js/index.js
@@ -1,25 +1,51 @@
 // Get the modal
-var modal = document.getElementById('signInModal');
+var signinmodal = document.getElementById('sign-in-modal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("signin-btn");
+var signinbtn = document.getElementById("sign-in-btn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var signinspan = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+signinbtn.onclick = function() {
+  signinmodal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+signinspan.onclick = function() {
+  signinmodal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == signinmodal) {
+    signinmodal.style.display = "none";
+  }
+}
+
+// Get the modal
+var signupmodal = document.getElementById('sign-up-modal');
+
+// Get the button that opens the modal
+var signupbtn = document.getElementById("sign-up-btn");
+
+// Get the <span> element that closes the modal
+var signupspan = document.getElementsByClassName("close")[1];
+
+// When the user clicks the button, open the modal 
+signupbtn.onclick = function() {
+  signupmodal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+signupspan.onclick = function() {
+  signupmodal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == signupmodal) {
+    signupmodal.style.display = "none";
   }
 }
