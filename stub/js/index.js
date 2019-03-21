7546ed4b238b5e53ae6e24a538e72b717e92a2f0
phanimkms <phanimkms@gmail.com>
2019-03-21 14:52:05 +0530

    Part A sol draft2 commit

--- stub/js/index.js
+++ stub/js/index.js
@@ -49,3 +49,41 @@ window.onclick = function(event) {
     signupmodal.style.display = "none";
   }
 }
+
+// Get the modal
+var createpostmodal = document.getElementById('create-post-modal');
+
+// Get the button that opens the modal
+var createpostbtn = document.getElementById("create-post-btn");
+
+// Get the <span> element that closes the modal
+var createpostspan = document.getElementsByClassName("close")[2];
+
+//When the user clicks on CreatePost, open the modal
+createpostbtn.onclick = function() {
+  createpostmodal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+createpostspan.onclick = function() {
+  createpostmodal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == createpostmodal) {
+    createpostmodal.style.display = "none";
+  }
+}
+
+
+//When the user clicks on AllPosts, redirect to bloglist.html
+
+//When the user clicks on Sign Up hyperlink in the Signin Modal
+$(document).ready(function() {
+
+  if(window.location.href.indexOf('#myModal') != -1) {
+    $('#myModal').modal('show');
+  }
+
+});
